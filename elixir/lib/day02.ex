57ae68df9f9c16553cde27b4599e9fd129ae7566
dimspith <51262483+dimspith@users.noreply.github.com>
2021-12-02 14:55:31 +0200

    Solve day2 in Elixir

---
+++ elixir/lib/day02.ex
@@ -0,0 +1,40 @@
+defmodule Day02 do
+  def get_input(file) do
+    File.read!(file)
+    |> String.split("\n", trim: true)
+  end
+
+  def compute_p1([op, val], {depth, distance}) do
+    val = String.to_integer(val)
+
+    case op do
+      "forward" -> {depth, distance + val}
+      "down" -> {depth + val, distance}
+      "up" -> {depth - val, distance}
+    end
+  end
+
+  def compute_p2([op, val], {depth, distance, aim}) do
+    val = String.to_integer(val)
+
+    case op do
+      "forward" -> {depth + aim * val, distance + val, aim}
+      "down" -> {depth, distance, aim + val}
+      "up" -> {depth, distance, aim - val}
+    end
+  end
+
+  def part1 do
+    get_input("priv/day02")
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.reduce({0, 0}, &compute_p1/2)
+    |> Tuple.product()
+  end
+
+  def part2 do
+    get_input("priv/day02")
+    |> Enum.map(&String.split(&1, " ", trim: true))
+    |> Enum.reduce({0, 0, 0}, &compute_p2/2)
+    |> (fn {depth, distance, _} -> depth * distance end).()
+  end
+end

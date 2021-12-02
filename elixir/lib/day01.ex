f19614e26e253d902014a9c67659e07e69f648d1
dimspith <51262483+dimspith@users.noreply.github.com>
2021-12-02 14:55:39 +0200

    Rework day1

--- elixir/lib/day01.ex
+++ elixir/lib/day01.ex
@@ -1,14 +1,14 @@
-defmodule Day1 do
+defmodule Day01 do
   def get_input(file) do
     File.read!(file)
     |> String.split("\n", trim: true)
-    |> Enum.map(&String.to_integer/1)
   end
 
   def part1 do
     input = get_input("priv/day01")
 
     input
+    |> Enum.map(&String.to_integer/1)
     |> Enum.zip(tl(input))
     |> Enum.count(fn {v1, v2} -> v2 > v1 end)
   end

1bf2056d625de8c95ef89e8c64ebe762d86a1803
dimspith <51262483+dimspith@users.noreply.github.com>
2021-12-03 23:29:01 +0200

    Solve day3 part2 in elixir and rework code

--- elixir/lib/day03.ex
+++ elixir/lib/day03.ex
@@ -2,65 +2,41 @@ defmodule Day03 do
   def get_input(file) do
     File.read!(file)
     |> String.split("\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
+    |> Enum.map(fn x -> Enum.map(x, &String.to_integer/1) end)
   end
 
-  def str_to_ints(s) do
-    s
-    |> String.graphemes()
-    |> Enum.map(&String.to_integer/1)
+  def get_bit(list, f) do
+    ones = Enum.sum(list)
+    if f.(ones, Enum.count(list) / 2), do: 1, else: 0
   end
 
-  def sort_freq(freq), do: Enum.sort_by(freq, fn {_, v} -> v end, :desc)
-
-  def extract_bit(list) do
-    freq =
-      list
-      |> Tuple.to_list()
-      |> Enum.frequencies()
-      |> sort_freq
-
-    [{mc, _}, {lc, _}] = freq
-    [mc, lc]
-  end
-
-  def find_bits(lists) do
+  def find_bits(lists, f) do
     lists
-    |> Enum.map(&str_to_ints/1)
-    |> List.zip()
-    |> Enum.map(&extract_bit/1)
-    |> List.zip()
+    |> Enum.zip()
     |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(fn list -> get_bit(list, f) end)
+    |> List.flatten()
   end
 
-  def find_rating(x, lists) do
-    IO.inspect(x)
-    if length(lists) <= 1 do
-      {:halt, List.flatten(lists)}
-    else
-      
-      {:cont, Enum.filter(lists, fn v -> v == x end)}
-    end
-  end
-
-  def life_support_rating([mc, lc], lists) do
-    co2 = Enum.reduce_while(mc, lists, &find_rating(&1, &2))
-    oxygen = Enum.reduce_while(lc, lists, &find_rating(&1, &2))
-    IO.inspect(co2, oxygen)
-    # Enum.product([co2, oxygen])
+  def filter_bits(lists, f) do
+    Enum.reduce_while(0..Enum.count(Enum.at(lists, 1)), lists, fn i, acc ->
+      bit = find_bits(acc, f) |> Enum.at(i)
+      new_lines = Enum.filter(acc, fn x -> Enum.at(x, i) == bit end)
+      if Enum.count(new_lines) == 1, do: {:halt, Enum.at(new_lines, 0)}, else: {:cont, new_lines}
+    end)
   end
 
   def part1 do
-    get_input("priv/day03")
-    |> find_bits
-    |> Enum.map(&Integer.undigits(&1, 2))
-    |> Enum.product()
+    most_common = get_input("priv/day03") |> find_bits(&(&1 >= &2))
+    least_common = Enum.map(most_common, &(Bitwise.bnot(&1) + 2))
+    Integer.undigits(most_common, 2) * Integer.undigits(least_common, 2)
   end
 
   def part2 do
-    input = get_input("priv/day03")
-    lists = input |> Enum.map(&str_to_ints/1)
-    bits = input |> find_bits
-    IO.inspect(bits)
-    life_support_rating(bits, lists)
+    lists = get_input("priv/day03")
+    co2 = filter_bits(lists, &(&1 >= &2))
+    oxygen = filter_bits(lists, &(&1 < &2))
+    Integer.undigits(co2, 2) * Integer.undigits(oxygen, 2)
   end
 end
